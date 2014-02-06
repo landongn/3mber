7b96cd2f1d5fa6f9bbd70b682262f40c355fa84a
Landon Noss <landongn@gmail.com>
2014-02-06 01:22:36 -0800

    more cleaning up, removing dead files, formatting, jshint blah blah

--- static/js/views/PeriodicTableElementView.js
+++ static/js/views/PeriodicTableElementView.js
@@ -1,14 +1,14 @@
 App.PeriodicTableElementView = App.Object3DView.extend({
-    templateName: 'element',
-    classNames: 'element',
-    didInsertElement: function () {
-        this.$().css({
-            position: 'absolute',
-            perspective: 'preserve-3d',
-            backgroundColor: 'rgba(0,127,127,' + (Math.random() * 0.5 + 0.25) + ')'
-        });
-        this.position.x = Math.random() * 4000 - 2000;
-        this.position.y = Math.random() * 4000 - 2000;
-        this.position.z = Math.random() * 4000 - 2000;
-    }
+	templateName: 'element',
+	classNames: 'element',
+	didInsertElement: function () {
+		this.$().css({
+			position: 'absolute',
+			perspective: 'preserve-3d',
+			backgroundColor: 'rgba(0,127,127,' + (Math.random() * 0.5 + 0.25) + ')'
+		});
+		this.position.x = Math.random() * 4000 - 2000;
+		this.position.y = Math.random() * 4000 - 2000;
+		this.position.z = Math.random() * 4000 - 2000;
+	}
 });
--- static/js/views/SceneView.js
+++ static/js/views/SceneView.js
@@ -1,22 +1,22 @@
 App.Scene = Ember.ContainerView.extend({
-    classNames: ['scene-view'],
-    init: function () {
-        this._super();
-        var scene = new App.three.Scene();
-        for (var prop in scene) {
-            if (this[prop]) {
-                var _prop = '__' + prop;
-                if (this[_prop]) {
-                    continue;
-                } else {
-                    this[_prop] = scene[prop];
-                    continue;
-                }
-            }
-            this.set(prop, scene[prop]);
-        }
-    },
-    didInsertElement: function () {
-        console.log("scene installed");
-    }
+	classNames: ['scene-view'],
+	init: function () {
+		this._super();
+		var scene = new App.three.Scene();
+		for (var prop in scene) {
+			if (this[prop]) {
+				var _prop = '__' + prop;
+				if (this[_prop]) {
+					continue;
+				} else {
+					this[_prop] = scene[prop];
+					continue;
+				}
+			}
+			this.set(prop, scene[prop]);
+		}
+	},
+	didInsertElement: function () {
+		console.log("scene installed");
+	}
 });
